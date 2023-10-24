0a3345222ca13ac8d75a41f803a77ec0b128d99f
molli1992 <fblaksley@rukasvancor.com>
2023-10-24 12:33:46 -0300

    telefono

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,57 +3,59 @@ import "./intro.css";
 import Vectorial from "../../logo/vector-removebg-preview.png";
 import Photo from "../../imagenes/caricatura-removebg-preview.png";
 
-
 function Intro() {
-
-
-    return (
-
-        <div className="intro">
-
-            <div className="i-left">
-
-                <div className="i-name">
-                    <span>Hy! I Am</span>
-                    <span>Felipe Blaksley</span>
-                    <span>
-                        Frontend Developer with high
-                        level of experiences in web desinging
-                        and development, producting the
-                        Quality work
-                    </span>
-                </div>
-
-                <button className="button i-button">Hire me</button>
-
-                <div className="i-icons">
-                    <a href="https://github.com/Molli1992" target="_blank" rel="noreferrer">
-                        <ion-icon name="logo-github"></ion-icon>
-                    </a>
-                    <a href="https://www.linkedin.com/in/felipe-blaksley-iraola-141b32236/" target="_blank" rel="noreferrer">
-                        <ion-icon name="logo-linkedin"></ion-icon>
-                    </a>
-                    <a href="https://www.instagram.com/felipeblaksley/" target="_blank" rel="noreferrer">
-                        <ion-icon name="logo-instagram"></ion-icon>
-                    </a>
-                    <a href="https://api.whatsapp.com/send?phone=1124586710" target="_blank" rel="noreferrer">
-                        <ion-icon name="logo-whatsapp"></ion-icon>
-                    </a>
-                </div>
-
-            </div>
-
-            <div className="i-right">
-
-                <img src={Vectorial} alt="Vectorial" className="vectorial" />
-                <img src={Photo} alt="Profile" className="i-photo-profile" />
-
-            </div>
-
+  return (
+    <div className="intro">
+      <div className="i-left">
+        <div className="i-name">
+          <span>Hy! I Am</span>
+          <span>Felipe Blaksley</span>
+          <span>
+            Frontend Developer with high level of experiences in web desinging
+            and development, producting the Quality work
+          </span>
         </div>
 
-    );
+        <button className="button i-button">Hire me</button>
 
-};
+        <div className="i-icons">
+          <a
+            href="https://github.com/Molli1992"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ion-icon name="logo-github"></ion-icon>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/felipe-blaksley-iraola-141b32236/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ion-icon name="logo-linkedin"></ion-icon>
+          </a>
+          <a
+            href="https://www.instagram.com/felipeblaksley/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ion-icon name="logo-instagram"></ion-icon>
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=5491124586710"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ion-icon name="logo-whatsapp"></ion-icon>
+          </a>
+        </div>
+      </div>
+
+      <div className="i-right">
+        <img src={Vectorial} alt="Vectorial" className="vectorial" />
+        <img src={Photo} alt="Profile" className="i-photo-profile" />
+      </div>
+    </div>
+  );
+}
 
 export default Intro;
