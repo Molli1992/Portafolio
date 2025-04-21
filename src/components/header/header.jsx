fa0a452883faf369a48c10598a7e2f8d4fd78cdf
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-04-21 08:16:14 -0300

    header button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,58 +1,57 @@
 import React from "react";
 import "./header.css";
-import { Link } from "react-scroll"
+import { Link } from "react-scroll";
 
 function header() {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Felipe Blaksley</div>
 
+        <div className="header-responsive-button">
+          <Link spy={true} to="Contact" smooth={true}>
+            <button className="header-button">Contact</button>
+          </Link>
+        </div>
+      </div>
 
-    return (
+      <div className="n-rigth">
+        <div className="n-list">
+          <ul>
+            <Link
+              spy={true}
+              to="header"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Home</li>
+            </Link>
 
-        <div className="n-wrapper">
+            <Link spy={true} to="Services" smooth={true}>
+              <li>Services</li>
+            </Link>
 
-            <div className="n-left">
-                <div className="n-name">Felipe Blaksley</div>
-            </div>
+            <Link spy={true} to="Experiences" smooth={true}>
+              <li>Experience</li>
+            </Link>
 
-            <div className="n-rigth">
+            <Link spy={true} to="Works" smooth={true}>
+              <li>Works</li>
+            </Link>
+            <Link spy={true} to="Portfolio" smooth={true}>
+              <li>Portfolio</li>
+            </Link>
+          </ul>
+        </div>
 
-                <div className="n-list">
-                    <ul>
-                        <Link spy={true} to="header" smooth={true} activeClass="activeClass" >
-                            <li>Home</li>
-                        </Link>
+        <Link spy={true} to="Contact" smooth={true}>
+          <button className="header-button">Contact</button>
+        </Link>
+      </div>
 
-                        <Link spy={true} to="Services" smooth={true} >
-                            <li>Services</li>
-                        </Link>
-
-                        <Link spy={true} to="Experiences" smooth={true} >
-                            <li>Experience</li>
-                        </Link>
-
-                        <Link spy={true} to="Works" smooth={true} >
-                            <li>Works</li>
-                        </Link>
-                        <Link spy={true} to="Portfolio" smooth={true} >
-                            <li>Portfolio</li>
-                        </Link>
-                    </ul>
-                </div >
-
-                <Link spy={true} to="Contact" smooth={true} >
-                    <button className="button">Contact</button>
-                </Link>
-
-            </div >
-
-            <div className="n-rigth-responsive">
-
-            </div>
-
-        </div >
-
-    );
-
-};
-
+      <div className="n-rigth-responsive"></div>
+    </div>
+  );
+}
 
 export default header;
